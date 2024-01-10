3599db94da8c0ed0b6fe16ab3c89fcc559305278
Rémi <>
2024-01-10 14:49:49 +0100

    spec instructions et traiter_ligne

--- src/instructions.ads
+++ src/instructions.ads
@@ -2,38 +2,20 @@ package Instruction is
 
 	type T_ABR is limited private;
 
-	Cle_Presente_Exception : Exception;	-- une clé est déjà présente dans un ABR
-	Cle_Absente_Exception  : Exception;	-- une clé est absente d'un ABR
-
 	-- Réaliser un branchement, mettre cp à la valeur souhaité 
 	procedure branchement(val_cp : in integer, cp : out integer) with
 		Post => cp = val_cp;
 
 
 	-- tester la condition et appeler branchement si condition valide 
-	procedure condition (test : T_Abr, val_cp : in integer, cp : out integer) ;
-
+    procedure condition (test : T_Abr, val_cp : in integer, cp : out integer) with
+        Post => test and (val_cp = cp);
+    
 
 	-- affecter une valeur à une variable 
 	procedure affectation (variable : in out T_variable, valeur : in integer) with
-		;
-
-
-
+        Post => variable'valeur = valeur;
 
 private
 
-	type T_Noeud;
-	type T_ABR is access T_Noeud;
-	type T_Noeud is
-		record
-			Cle: Character;
-			Donnee : Integer;
-			Sous_Arbre_Gauche : T_ABR;
-			Sous_Arbre_Droit : T_ABR;
-			-- Invariant
-			--    Pour tout noeud N dans Sous_Arbre_Gauche, N.Cle < Cle
-			--    Pour tout noeud N dans Sous_Arbre_Droit,  N.Cle > Cle
-		end record;
-
-end ABR;
+end Instruction;
--- src/traiter_ligne.ads
+++ src/traiter_ligne.ads
@@ -1,1 +1,13 @@
 package TraiterLigne is
+
+    CAPACITE : constant Integer := 4;      -- nombre maximum d'instructions par ligne
+
+
+    procedure executer_ligne(instruction : in T_Instruction) ;
+
+
+private
+
+    type T_Instruction is array (1..CAPACITE) of T_Materiel;
+
+end TraiterLigne;
