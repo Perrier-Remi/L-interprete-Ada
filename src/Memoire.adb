e8de5cab5847fe1b7865b83a13fcb35a420cc6dc
mathis <mathisbanzet@orange.fr>
2024-01-14 14:34:03 +0100

    Package memoire fonctionnelle paramétre non privé ads + adb + Fichier Test_Memoire

---
+++ src/Memoire.adb
@@ -0,0 +1,91 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO;          use Ada.Text_IO;
+with Ada.Integer_Text_IO;  use Ada.Integer_Text_IO;
+
+package body Memoire is
+
+	-- Initialiser la structure de donné composé d'un tableau et d'une Valeur Taille qui nous indique la taille du tableau défini
+	--procedure Initialiser (Variable : out T_Variable) is
+   --begin
+   --   Variable.Taille := 0;
+   --end Initialiser;
+   
+
+   procedure Initialiser (Variable : out T_Variable) is
+   begin
+      Variable.Taille := 0;
+      Variable.Tab_var := Creer_Tab_Variable;
+   end Initialiser;
+
+   
+
+   function Creer_Tab_Variable return T_Tab_Variable is
+   Variable : T_Tab_Variable := (others => (Nom => To_Unbounded_String(""), Valeur => 0, Code => 0));
+   begin
+      return Variable;
+   end Creer_Tab_Variable;
+
+
+function Creer_Donee_Variable (Code : Integer; Valeur : Integer; Nom : Unbounded_String) return T_Donee_Variable is
+   Variable : T_Donee_Variable;
+begin
+   Variable.Code := Code;
+   Variable.Valeur := Valeur;
+   Variable.Nom := Nom;
+   return Variable;
+end Creer_Donee_Variable;
+
+   -- Créer une variable avec son code, sa valeur et son nom passé en paramètre
+   procedure Creer_Variable (Code : Integer; Valeur : Integer; Nom : Unbounded_String; Variable : in out T_Variable) is
+   begin
+      Variable.Taille := Variable.Taille + 1;
+      Variable.Tab_var(Variable.Taille) := Creer_Donee_Variable(Code, Valeur, Nom);
+   end Creer_Variable;
+	
+   
+	-- Affecter la variable avec la valeur passé en paramètre et appelle de la fonction affecter du bon package
+  procedure Affectation_Variable (Code : in integer; Valeur : in integer; Variable : in out T_Variable) is
+   begin
+      -- Rechercher la variable correspondante dans le tableau
+      for I in 1..Variable.Taille loop
+         if Variable.Tab_var(I).Code = Code then
+            -- Affecter la nouvelle valeur à la variable
+            Variable.Tab_var(I).Valeur := Valeur;
+         end if;
+      end loop;
+   end Affectation_Variable;
+
+   --Renvoie la variable correspondante au code passé en paramètre
+   function Renvoie_Variable (Code : in integer; Variable : in T_Variable) return T_Donee_Variable is
+   Result : T_Donee_Variable; 
+   begin
+      -- Rechercher la variable correspondante dans le tableau
+      for I in 1.. Variable.Taille loop
+         if Variable.Tab_var(I).Code = Code then
+            Result := Variable.Tab_var(I);
+            exit;
+         end if;
+      end loop;
+      return Result;
+   end Renvoie_Variable;
+
+   --Renvoie tous le tableau de variable
+   function Renvoie_Tab_Variable (Variable : in T_Variable) return T_Tab_Variable is
+   begin
+      return Variable.Tab_var;
+   end Renvoie_Tab_Variable;
+
+   --Renvoie la valeur maximun du code, le code maximun est stocké dans le dernier enregistrement
+   function Renvoie_Code_Max (Variable : in T_Variable) return Integer is
+   begin
+         return Variable.Taille;
+   end Renvoie_Code_Max;
+   
+   function Renvoie_Taille (Variable : in T_Variable) return Integer is
+   begin
+         return Variable.Taille;
+   end Renvoie_Taille;
+   
+
+
+end Memoire;
---
+++ src/Test_Memoire.adb
@@ -0,0 +1,62 @@
+with Memoire; use Memoire;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Test_Memoire is
+   -- Déclarer une variable pour tester le package Memoire
+   Ma_Variable : T_Variable;
+   Tab_Variable_Result : T_Tab_Variable;
+   Ma_Variable_Result : T_Donee_Variable;
+
+   -- Déclarer d'autres variables nécessaires pour les tests
+   Code_Test_1 : constant Integer := 1;
+   Valeur_Test_1 : constant Integer := 42;
+   Nom_Test_1 : Unbounded_String := To_Unbounded_String("Variable_Test");
+   Code_Test_2 : constant Integer := 2;
+   Valeur_Test_2 : constant Integer := 26;
+   Nom_Test_2 : Unbounded_String := To_Unbounded_String("Variable_B");
+
+
+begin
+   -- Initialiser Memoire
+   Initialiser(Ma_Variable);
+
+   --Test taille mémoire
+   Put_Line("Taille de T_Variable : " & Integer'Image(Renvoie_Taille(Ma_Variable)));
+   pragma Assert (Renvoie_Taille(Ma_Variable) = 0);
+
+   --Implementer deux variable
+   Creer_Variable(Code_Test_1, Valeur_Test_1, Nom_Test_1, Ma_Variable);
+   Creer_Variable(Code_Test_2, Valeur_Test_2, Nom_Test_2, Ma_Variable);
+
+   --Test taille mémoire instancié
+   Put_Line("Taille de T_Variable : " & Integer'Image(Renvoie_Taille(Ma_Variable)));
+   pragma Assert (Renvoie_Taille(Ma_Variable) = 2);
+
+   --Recupérer la variable de code 1
+   Ma_Variable_Result := Renvoie_Variable(Code_Test_1, Ma_Variable);
+   pragma Assert (Ma_Variable_Result.Code = Code_Test_1);
+   pragma Assert (Ma_Variable_Result.Valeur = Valeur_Test_1);
+   pragma Assert (To_String(Ma_Variable_Result.Nom) = To_String(Nom_Test_1));
+
+   --Modifier valeur variable code = 1
+   Affectation_Variable(Code_Test_1, Valeur_Test_1 + 10, Ma_Variable);
+
+   --Recupérer tableau de Tab_Variable
+   Tab_Variable_Result := Renvoie_Tab_Variable(Ma_Variable);
+
+   -- Afficher le tableau de variable
+   Put_Line("Affichage du tabeau des Variable :");
+   for I in 1..Renvoie_Taille(Ma_Variable) loop
+         Put_Line("Variable " & Integer'Image(I) & " :");
+         Put_Line("Code : " & Integer'Image(Tab_Variable_Result(I).Code));
+         Put_Line("Valeur : " & Integer'Image(Tab_Variable_Result(I).Valeur));
+         Put_Line("Nom : " & To_String(Tab_Variable_Result(I).Nom));
+      end loop;
+
+   --Afficher le code max et donc aussi la taille de la mémoire
+   Put_Line("Le code max est"& Integer'Image(Renvoie_Code_Max(Ma_Variable)));
+   pragma Assert (Renvoie_Code_Max(Ma_Variable) = 2);
+
+end Test_Memoire;
