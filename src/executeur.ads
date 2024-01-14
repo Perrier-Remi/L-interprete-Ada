9c041a98be2918ccc84598b32d583835a73b961e
Rémi <>
2024-01-14 18:39:42 +0100

    tests executeur

--- src/executeur.ads
+++ src/executeur.ads
@@ -1,20 +1,27 @@
+with Memoire;
+
 package Executeur is
 
 	-- Réaliser un branchement, mettre cp à la valeur souhaité 
-	procedure branchement(val_cp : in integer, cp : out integer) with
+	function branchement(valCp : in integer, nouveauCp : in integer) return Integer with
 		Post => cp = val_cp;
 
 
-	-- tester la condition et appeler branchement si condition valide 
-    procedure condition (test : T_Abr, val_cp : in integer, cp : out integer) with
+	-- tester la condition et appeler branchement cp si condition valide sinon exécuter la ligne suivante
+    procedure condition (test : integer, valCp : in integer, nouveauCp : in integer) return integer with
         Post => test and (val_cp = cp);
     
 
     -- affecter une valeur à une variable
-    procedure affectation (nom_variable : in string, valeur : in integer);
+    procedure affectation (memoire : in out T_Variable, variable : in integer, valeur : in integer) with
+        Post => Renvoie_Variable(variable, memoire)'Valeur = valeur;
     
-    -- 
-    procedure operation(var_1 : in T_variable, var_2 : in T_variable, operateur : in string);
+    
+    -- effectuer une opération avec deux variables sources et écrire le résultat dans une variable destination
+    -- src1 et src2 peuvent représenter une variable ou une constante
+    -- src1 est une constante si src1Const est vrai (1) sinon (0) src1 représente une variable, pareil pour src2
+    procedure operation(memoire : in out T_Variable, varDest : in Integer, src1 : in Integer, src2 : in Integer, operateur : in integer, src1Const : in integer, src2Const : in integer);
+    
     
     
 end Executeur;
--- src/interpreteur.ads
+++ src/interpreteur.ads
@@ -1,13 +1,13 @@
 package Interpreteur is
 
-    CAPACITE : constant Integer := 4;      -- nombre maximum d'instructions par ligne
+    NB_INSTRUCTIONS : Constant Integer := 6;      -- nombre maximum d'instructions par ligne
 
-
+    -- procedure permettant d'executer une ligne du programme
     procedure executer_ligne(instruction : in T_Instruction) ;
 
 
 private
 
-    type T_Instruction is array (1..CAPACITE) of string;
+    type T_Instruction is array (1..NB_INSTRUCTIONS) of Integer;
 
 end Interpreteur;
